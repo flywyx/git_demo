78417ffd5a602a9530850ee03a7cbdcd4eb0625a
wuyx6 <wuyx6@lenovo.com>
2015-05-29 15:41:58 +0800

    new project

---
+++ src/com/qq/Test.java
@@ -0,0 +1,15 @@
+package com.qq;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="hello";
+		System.out.print(str);
+		
+	}
+
+}
